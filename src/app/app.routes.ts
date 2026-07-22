d1f0ce4571063abcd52d3a17aee183154e502a31
Milenny Bezerra <milennybezerra8@gmail.com>
2026-07-22 19:08:51 -0300

    refactor: src/app/app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { ListaProdutos } from '../app/features/produtos/lista-produtos/lista-produtos';
+import { Carrinho } from './features/carrinho/carrinho/carrinho';
+import { Home } from './features/home/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+          component: Home,
+    },
+    {
+        path:'produto',
+          component: ListaProdutos,
+    },
+    {
+        path:'carrinho',
+          component: Carrinho,
+    },
+    {
+        path:'**',
+          redirectTo:'',
+    },
+];
+   
