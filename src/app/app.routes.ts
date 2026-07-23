3705e4e6e50ac9c33482125045864fef0dce7c7c
Milenny Bezerra <milennybezerra8@gmail.com>
2026-07-22 21:29:04 -0300

    refector:  src/app/app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,28 @@
-import { Routes } from '@angular/router';
-import { ListaProdutos } from '../app/features/produtos/lista-produtos/lista-produtos';
-import { Carrinho } from './features/carrinho/carrinho/carrinho';
-import { Home } from './features/home/home/home';
+import { Routes } from "@angular/router";
+import { authGuard } from "./core/auth.guard";
 
 export const routes: Routes = [
-    {
-        path:'',
-          component: Home,
-    },
-    {
-        path:'produto',
-          component: ListaProdutos,
-    },
-    {
-        path:'carrinho',
-          component: Carrinho,
-    },
-    {
-        path:'**',
-          redirectTo:'',
-    },
+  {
+    path:'', //! router para raiz localhost:4200/
+    loadComponent: () =>
+      import('./features/home/home/home')
+    .then((m) => m.Home),
+  },
+  {
+    path:'produtos',
+    loadComponent: () =>
+      import('./features/produtos/lista-produtos/lista-produtos')
+    .then((m) => m.ListaProdutos),
+  },
+  {
+    path:'carrinho',
+    canActivate: [authGuard],
+    loadComponent: () =>
+      import('./features/carrinho/carrinho/carrinho')
+    .then((m) => m.Carrinho)
+  },
+  {
+    path:'**',
+    redirectTo: '',
+  },
 ];
-   
